e29c65aed584fcca10caaab4c96b01b23cb6231b
Geir André Lund <geir.andre.lund@nav.no>
2021-08-26 09:32:50 +0200

    kubectl alias

--- kubernetes/aliases.zsh
+++ kubernetes/aliases.zsh
@@ -3,6 +3,8 @@ alias k='kubectl'
 alias pods='{name=$(cat -); k get pods --all-namespaces -lapp=$name; }<<<'
 
 alias dev-fss='kubectx dev-fss'
+alias dev-sbs='kubectx dev-sbs'
+alias prod-sbs='kubectx prod-sbs'
 alias prod-fss='kubectx prod-fss'
 alias dev-gcp='kubectx dev-gcp'
 alias prod-gcp='kubectx prod-gcp'
