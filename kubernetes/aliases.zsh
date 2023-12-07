8bf8d2fa4da2332902a431bd752c653881e9dd1f
Geir André Lund <geir.andre.lund@nav.no>
2023-12-07 12:14:30 +0100

    Fjerne ubrukt commando

--- kubernetes/aliases.zsh
+++ kubernetes/aliases.zsh
@@ -1,7 +1,4 @@
 alias k='kubectl'
-
-alias pods='{name=$(cat -); k get pods --all-namespaces -lapp=$name; }<<<'
-
 alias dev-fss='kubectx dev-fss'
 alias prod-fss='kubectx prod-fss'
 alias dev-gcp='kubectx dev-gcp'
