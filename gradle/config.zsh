59891bb0b641390bbbbae4505149e16be7cfc27b
Geir André Lund <geir.andre.lund@nav.no>
2023-12-07 12:12:37 +0100

    Eksponere gradle secrets via kommando.

--- gradle/config.zsh
+++ gradle/config.zsh
@@ -1,4 +1,6 @@
 export GRADLE_OPTS="-Xms2g -Xmx4048m -Xss16m -XX:+HeapDumpOnOutOfMemoryError -Dfile.encoding=UTF-8"
 
-export ORG_GRADLE_PROJECT_githubUser="x-access-token"
-export ORG_GRADLE_PROJECT_githubPassword=`op read "op://Jobb/GithubPat/credential"` # https://support.1password.com/command-line-getting-started/
+gradle_secrets() {
+  export ORG_GRADLE_PROJECT_githubUser="x-access-token"
+  export ORG_GRADLE_PROJECT_githubPassword=`op read "op://Jobb/GithubPat/credential"` # https://support.1password.com/command-line-getting-started/
+}
