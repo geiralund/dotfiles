1607b3653af9b23b09948a4d94e5a36d65709349
Geir André Lund <geir.andre.lund@nav.no>
2023-12-29 07:03:11 +0100

    Sjekker om en må eksportere.

--- gradle/config.zsh
+++ gradle/config.zsh
@@ -1,6 +1,12 @@
 export GRADLE_OPTS="-Xms2g -Xmx4048m -Xss16m -XX:+HeapDumpOnOutOfMemoryError -Dfile.encoding=UTF-8"
 
-gradle_secrets() {
-  export ORG_GRADLE_PROJECT_githubUser="x-access-token"
+
+export ORG_GRADLE_PROJECT_githubUser="x-access-token"
+if [ -z "$ORG_GRADLE_PROJECT_githubPassword" ]; then
   export ORG_GRADLE_PROJECT_githubPassword=`op read "op://Jobb/GithubPat/credential"` # https://support.1password.com/command-line-getting-started/
-}
+  export GITHUB_USERNAME=$ORG_GRADLE_PROJECT_githubUser
+  export GITHUB_PASSWORD=$ORG_GRADLE_PROJECT_githubPassword
+fi
+
+
+
