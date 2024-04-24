f41c499bf03a36267c598ab77d75cf7dbcd59490
Geir André Lund <geir.andre.lund@nav.no>
2024-04-24 13:35:26 +0200

    Litt opprydding.

--- github/pat-config.zsh
+++ github/pat-config.zsh
@@ -2,7 +2,8 @@ github_pat() {
   if [ -z "$PAT_SECRET" ]; then
     echo "Henter Github PAT fra 1Password"
     export PAT_SECRET=$(op read "op://Jobb/GithubPat/credential") # https://support.1password.com/command-line-getting-started/
-  else
+    call reload! # Reloads the shell
+ else
     echo "Github PAT fra 1Password er allerede satt"
   fi
 }
--- gradle/config.zsh
+++ gradle/config.zsh
@@ -1,5 +1,6 @@
 export GRADLE_OPTS="-Xms2g -Xmx4048m -Xss16m -XX:+HeapDumpOnOutOfMemoryError -Dfile.encoding=UTF-8"
 export ORG_GRADLE_PROJECT_githubUser="x-access-token"
 export ORG_GRADLE_PROJECT_githubPassword=$PAT_SECRET
-export GITHUB_USERNAME=$ORG_GRADLE_PROJECT_githubUser
-export GITHUB_PASSWORD=$ORG_GRADLE_PROJECT_githubPassword
+export GITHUB_USERNAME="x-access-token"
+export GITHUB_PASSWORD=$PAT_SECRET
+export GITHUB_TOKEN=$PAT_SECRET
