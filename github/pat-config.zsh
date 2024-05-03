985335f3fe2d2a3ac98cf8743c2db53e3e295954
Geir André Lund <geir.andre.lund@nav.no>
2024-05-03 08:12:13 +0200

    fiks

--- github/pat-config.zsh
+++ github/pat-config.zsh
@@ -1,9 +1,10 @@
 github_pat() {
   if [ -z "$PAT_SECRET" ]; then
     echo "Henter Github PAT fra 1Password"
-    export PAT_SECRET=$(op read "op://Jobb/GithubPat/credential") # https://support.1password.com/command-line-getting-started/
-    call reload! # Reloads the shell
- else
+    PAT_SECRET=$(op read "op://Jobb/GithubPat/credential") # https://support.1password.com/command-line-getting-started/
+    export PAT_SECRET
+    reload!
+  else
     echo "Github PAT fra 1Password er allerede satt"
   fi
 }
