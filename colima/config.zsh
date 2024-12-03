8864d8a19ee86fbed03324c470aa179227d842c0
Geir André Lund <geir.andre.lund@nav.no>
2024-12-03 09:55:04 +0100

    Peke på riktig docker host.

--- colima/config.zsh
+++ colima/config.zsh
@@ -1,5 +1,5 @@
 # Colima
 export TESTCONTAINERS_RYUK_DISABLED=true
 export TESTCONTAINERS_DOCKER_SOCKET_OVERRIDE=/var/run/docker.sock
-export DOCKER_HOST=unix://$HOME/.colima/docker.sock
+export DOCKER_HOST=unix://$HOME/.colima/default/docker.sock
 
